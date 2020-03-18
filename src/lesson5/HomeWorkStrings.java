4bc9e9f951ba3522db6b9c0e2c021b898048dddb
kvashninaOlga <olgakvashnina@engineer.com>
2020-03-18 17:00:22 +0300

    lesson5

--- src/lesson5/HomeWorkStrings.java
+++ src/lesson5/HomeWorkStrings.java
@@ -6,8 +6,8 @@ public class HomeWorkStrings {
     public static void main(String[] args) {
 
 
-//    1.Даны 2 слова, состоящие из четного числа букв.
-//    Получить слово состоящее из первой половины первого слова и второй половины второго слова.
+//       1.Даны 2 слова, состоящие из четного числа букв.
+//      Получить слово состоящее из первой половины первого слова и второй половины второго слова.
         String string1 = "coronavirs";
         String string2 = "pandemix";
         String string3 = string1.substring(0, string1.length() / 2);
@@ -21,40 +21,64 @@ public class HomeWorkStrings {
         System.out.println(str5);
         System.out.println("-----");
 
-//    2.Найдите самое длинное слово в предложении, при условии, что в предложении все слова разной длины.
+//        4.Написать функцию, которая проверяет, является ли строка палиндромом.
+//        Палиндром — это слово или фраза, которые одинаково читаются по буквам или по словам как слева направо, так и справа налево.
 
-            String str1 = "I said not to Covid-19";
-            String[] words = str1.split(" ");
-            System.out.println("The longest word is: " + maxLengthWord(words));
-        }
-        private static String maxLengthWord (String[]str){
-            int length = 0;
-            String longest = "";
-            for (String elem : str) {
-                if (elem.length() > length) {
-                    length = elem.length();
-                    longest = elem;
+        String s = "nononon";
+        boolean bool1 = true;
+        if (s.length() % 2 == 0) {
+            for (int i = 0; i < s.length() / 2; ++i) {
+                if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                    bool1 = false;
                 }
             }
-            return longest;
+        } else {
+            for (int i = 0; i < (s.length() - 1) / 2; ++i) {
+                if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                    bool1 = false;
+                }
+            }
+        }
+        if (bool1 == false) {
+            System.out.println("Not a polyndrom");
+        } else {
+            System.out.println("Is polyndrom");
+        }
+
+
+//    2.Найдите самое длинное слово в предложении, при условии, что в предложении все слова разной длины.
+
+        String str1 = "I said not to Covid-19";
+        String[] words = str1.split(" ");
+        System.out.println("In " + Arrays.toString(words));
+        System.out.println("the longest word is: " + maxLengthWord(words));
+    }
+
+    public static String maxLengthWord(String[] str) {
+        int length = 0;
+        String longest = "";
+        for (String elem : str) {
+            if (elem.length() > length) {
+                length = elem.length();
+                longest = elem;
+            }
+        }
+        return longest;
             /*for (int i = 0; i < strings.length; i++) {
             str = strings[i];
-            if (strings[i].length() >= strings[i+1].length()) {
+            if (strings[i].length() == maxLenght) {
                 str = strings[i+1];
                 System.out.println(str);*/
-        }
+
+    }
 }
 
 
-//    3.Имеются две строки. Найти количество вхождений одной строки в другую.
-//    4.Написать функцию, которая проверяет, является ли строка палиндромом.
-//    Палиндром — это слово или фраза, которые одинаково читаются по буквам или по словам как слева направо, так и справа налево.
-//    5.Даны два слова и словарь (массив слов). Необходимо найти алгоритм превращения в другое, меняя за один шаг одну букву,
-//    причем каждое новое слово должно быть в словаре (массиве слов).
-//    Например,даны слова "hit" и "cog" и словарь(массив слов) ["hot", "dot", "dog", "log", "lot"].
-//    Один из вариантов цепочки: "hit"->"hot"->"dot"->"dog"->"cog".
-//    6.Пользователь вводит названия городов через пробел. Вы их присваиваете переменной.
-//    Переставьте названия так, чтобы они были упорядочены по алфавиту.*/
+
+
+
+
+
 
 
 
